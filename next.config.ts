fa8d73ff9bce55fc3c01f8af5b6dc00ead3b6b95
Yevhen Onopriienko <fromzpcity@gmail.com>
2026-09-05 19:09:22 +0300

    04. usefull-things-439-text. Upgrade UI. Responsive. Fix deploy.

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,8 @@
 import type { NextConfig } from "next";
 
-const repo = "usefull-things-439-text";
-
 const nextConfig: NextConfig = {
     /* config options here */
     devIndicators: false,
-
-    output: "export",
-    basePath: process.env.NODE_ENV === "production" ? `/${repo}` : "",
-    assetPrefix: process.env.NODE_ENV === "production" ? `/${repo}/` : "",
-    images: {
-        unoptimized: true,
-    },
 };
 
 export default nextConfig;
